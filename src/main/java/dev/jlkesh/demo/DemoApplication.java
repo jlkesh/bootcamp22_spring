ed0972a6467a8db84f8ed6cba02f634c6289e415
pdp <john.lgd65@gmail.com>
2023-02-23 18:08:24 +0500

    initial commit

--- src/main/java/dev/jlkesh/demo/DemoApplication.java
+++ src/main/java/dev/jlkesh/demo/DemoApplication.java
@@ -1,11 +1,17 @@
 package dev.jlkesh.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -73,50 +79,74 @@ class BookController {
         return "redirect:/books";
     }
 
+
+    @GetMapping(value = "/books/update/{id}")
+    public String bookUpdatePage(Model model, @PathVariable String id) {
+        Book book = bookService.findById(id);
+        model.addAttribute("book", book);
+        return "book/update";
+    }
+
+    @PostMapping(value = "/books/update/{id}")
+    public String bookUpdatePage(@ModelAttribute BookCreateVO bookCreateVO, @PathVariable String id) {
+        bookService.update(id, bookCreateVO);
+        return "redirect:/books";
+    }
+
 }
 
 
-@Service
-record BookService() {
+interface BookRepository extends JpaRepository<Book, String> {
+    List<Book> findAllByAuthor(String a);
+}
 
-    public static final List<Book> BOOKS = new ArrayList<>() {{
-        add(new Book(UUID.randomUUID().toString(), "Spring For Beginners", "ME"));
-        add(new Book(UUID.randomUUID().toString(), "Spring Core", "ME"));
-        add(new Book(UUID.randomUUID().toString(), "Spring AOP", "ME"));
-    }};
+@Service
+record BookService(BookRepository repository) {
 
     public List<Book> getAll() {
-        return BOOKS;
+        return repository.findAll();
     }
 
     public Book findById(String id) {
         Supplier<RuntimeException> supplier = () -> new RuntimeException("Book not found by id '%s'".formatted(id));
-        return BOOKS.stream().filter(book -> book.id().equals(id))
-                .findAny()
-                .orElseThrow(supplier);
+        return repository.findById(id).orElseThrow(supplier);
     }
 
-    public String create(@NonNull BookCreateVO bookCreateVO) {
-        Book book = bookCreateVO.toBook();
-        BOOKS.add(book);
-        return book.id();
+    public String create(@NonNull BookCreateVO vo) {
+        Book book = Book.builder()
+                .title(vo.title())
+                .author(vo.author())
+                .build();
+        repository.save(book);
+        return book.getId();
     }
 
     public boolean delete(String id) {
-        return BOOKS.removeIf(book -> book.id().equals(id));
+        repository.deleteById(id);
+        return true;
+    }
+
+    public void update(String id, BookCreateVO vo) {
+        Book updatingBook = new Book(id, vo.title(), vo.author());
+        repository.save(updatingBook);
     }
 }
 
 
-record Book(String id, String title, String author) {
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+final class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String title;
+    private String author;
 }
 
 record BookCreateVO(String title, String author) {
-    public Book toBook() {
-        return new Book(
-                UUID.randomUUID().toString(),
-                this.title,
-                this.author
-        );
-    }
 }
